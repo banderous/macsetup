ed15210ffd9264db5ca9484eb504a067a13adeee
Alex McAusland <a.m@user.com>
2022-08-15 11:13:17 +0100

    ln

--- setup.sh
+++ setup.sh
@@ -1,18 +1,18 @@
 set -e
-ln -s .bash_profile ~/
-ln -s .gitconfig ~/
-ln -s .vimrc ~/
-ln -s .ideavimrc ~/
+ln -sf "$(pwd)/.bash_profile" ~/
+ln -sf "$(pwd)/.gitconfig" ~/
+ln -sf "$(pwd)/.vimrc" ~/
+ln -sf "$(pwd)/.ideavimrc" ~/
 mkdir -p ~/.vim/colors
-ln -s solarized.vim ~/.vim/colors/
+ln -sf "$(pwd)/solarized.vim" ~/.vim/colors/
 
 read -p "Enter email: " email
 ssh-keygen -t ed25519 -C "$email"
 eval "$(ssh-agent -s)"
-ln -s sshconfig ~/.ssh/config
+ln -sf "$(pwd)/sshconfig" ~/.ssh/config
 
 sudo mkdir -p -m 775 /usr/local/bin
-sudo ln -s g /usr/local/bin/
+sudo ln -sf "$(pwd)/g" /usr/local/bin/
 xcode-select --install
 sudo /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
@@ -33,7 +33,7 @@ brew install --cask gimp
 brew install vim
 
 mkdir -p ~/.config/karabiner/assets/complex_modifications/
-ln -s karabiner/karabiner.json ~/.config/karabiner/karabiner.json
+ln -sf "$(pwd)/karabiner/karabiner.json" ~/.config/karabiner/karabiner.json
 cp -r karabiner/assets/complex_modifications/ ~/.config/karabiner/assets/complex_modifications/
 
 curl -s "https://get.sdkman.io" | bash
